ded421ac2d20247d899843b1be6904823aedb17b
hanuman <hanu.s@jothisoftware.com>
2025-04-25 13:59:20 +0530

    - dynamic api call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
             .then(data => setMessage(data.message));
     }, []);
 
-    return <h1>{message || 'Loading...'} {api}</h1>;
+    return <h1>{message || 'Loading....'} {api}</h1>;
 }
 
 export default App
